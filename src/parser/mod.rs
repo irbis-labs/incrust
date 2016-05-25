4786d3e3ac799ea6875ad01b4e9eeba955696daf
alexander.irbis <irbis.labs@gmail.com>
2016-05-25 20:14:59 +0300

    code cleanup

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -6,10 +6,8 @@ pub mod block_level;
 pub mod expressions;
 pub mod literals;
 pub mod statements;
-pub mod util;
 
 pub use self::block_level::*;
 pub use self::expressions::*;
 pub use self::literals::*;
 pub use self::statements::*;
-pub use self::util::*;
