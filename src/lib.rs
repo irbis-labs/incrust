11ac772bd0d4676fa4f935ab69a127ee4d33cedb
Alexander Irbis <irbis.labs@gmail.com>
2017-04-04 11:15:25 +0000

    REMOVE clippy dependency; use `cargo clippy` instead

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,8 @@
 #![feature(box_syntax)]
 #![feature(specialization)]
 
-#![cfg_attr(feature = "clippy", feature(plugin))]
-#![cfg_attr(feature = "clippy", plugin(clippy))]
-#![cfg_attr(feature = "clippy", allow(items_after_statements, match_bool))]
+#![cfg_attr(feature = "clippy", allow(items_after_statements))]
+#![cfg_attr(feature = "clippy", allow(match_bool))]
 
 #![cfg_attr(feature = "quiet", allow(warnings))]
 
